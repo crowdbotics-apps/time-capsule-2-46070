a7697f9224fb622bef2524cba91acb88f73481f6
Crowdbotics <team@crowdbotics.com>
2024-02-07 16:35:43 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "time-capsule-2-46070",
+  url: "https://time-capsule-2-46070.botics.co",
+  api: "https://time-capsule-2-46070.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
